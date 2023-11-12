1a2e8091009ed5163d3dcc09f07eea43e44fe04e
Aaron Pierson <me@aaronpierson.me>
2023-11-11 23:34:41 -0600

    Home page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,91 @@
-import Image from 'next/image'
+// Import necessary modules and styles
+import Image from 'next/image';
+import Link from 'next/link';
+
+// Import your company logo image
+// import logoImage from '../public/imgs/logos/white.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col items-center justify-center text-white">
+      {/* Header section */}
+      <section className="text-center bg-cyan-950 w-full h-80 flex flex-col items-center justify-center text-white">
+      <header className="mb-8">
+        {/* <Image src={logoImage} alt="Company Logo" width={250} height={250} /> */}
+        <h1 className="text-4xl font-bold">Mana Worlds</h1>
+        <p className="mt-2">Innovation in Gaming</p>
+      </header>
+      </section>
+
+      {/* About section */}
+      <section className="text-center bg-zinc-50 w-full h-fit flex flex-col items-center justify-center text-cyan-950">
+        <h2 className="text-2xl font-bold m-5">Who is Mana Worlds</h2>
+        <div className='flex flex-row w-full columns-3 m-10 items-center'>
+          <div className="w-1/2" >
+            {/* <Image className="m-auto" src="/imgs/Icons/accent1-transparent.png" alt="Company Logo" width={400} height={400} /> */}
+          </div>
+
+          <div className="divider divider-horizontal"></div>
+
+          <div className='w-1/2 m-8 text-left'>
+            <p className="text-md m-4">
+              Mana Worlds is a software development company that strives to create unforgettable gaming experiences. 
+              We are passionate about gaming and are committed to delivering the best gaming experiences to our players. Our team of is made up of talented developers, designers, and gamers who work together to create games that are both fun and challenging. 
+              We believe that gaming is more than just a hobby; its a way of life. 
+              Thats why we are dedicated to creating games that are not only entertaining but also meaningful. 
+              Whether youre a casual gamer or a hardcore gamer, we have something for everyone. 
+              Join us on our journey to create the best gaming experiences in the world.
+            </p>
+            <Link href="/about">
+              <div className="bg-cyan-800 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">
+                More About Us
+              </div>
+            </Link>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Other Games section */}
+      <section className="text-center bg-cyan-950 w-full h-80 flex flex-col items-center justify-center text-white">
+        <p className="text-lg">Explore our diverse portfolio of games that cater to every gaming taste.</p>
+        <Link href="/games">
+          <div className="bg-cyan-800 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">
+            View Our Games
+          </div>
+        </Link>
+      </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {/* Community section */}
+      <section className="text-center bg-zinc-50 w-full h-80 flex flex-col items-center justify-center text-cyan-950">
+        <p className="text-lg">Join our gaming community to connect with fellow gamers, share experiences, and stay updated.</p>
+        <Link href="/community">
+          <div className="bg-cyan-800 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">
+            Join Community
+          </div>
+        </Link>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      {/* News section */}
+      <section className="text-center bg-cyan-950 w-full h-80 flex flex-col items-center justify-center text-white">
+        <p className="text-lg">Stay in the loop with the latest news, updates, and announcements from Your Game Company Name.</p>
+        <Link href="/news">
+          <div className="bg-cyan-800 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">
+            Read News
+          </div>
+        </Link>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+      {/* Media section (Optional) */}
+      {/* Add this section if you want a dedicated page for media like images, videos, etc. */}
+      {/* <section className="text-center mb-8">
+        <p className="text-lg">Explore our media gallery featuring stunning visuals and gameplay footage.</p>
+        <Link href="/media">
+          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4 inline-block">
+            View Media
+          </div>
+        </Link>
+      </section> */}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from 'next/link'
+
+
+const Navbar: React.FC = () => {
+  return (
+    <header>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                <label tabIndex={0} className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                </label>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><Link href='/'>Homepage</Link></li>
+                    <li><Link href='/about'>About</Link></li>
+                    <li><a>Games</a></li>
+                    <li><a>News</a></li>
+                    <li><a>Community</a></li>
+                    <li><a>Contact</a></li>
+                </ul>
+                </div>
+            </div>
+            <div className="navbar-end">
+                <Link href='/' className="btn btn-ghost normal-case text-xl">Mana Worlds</Link>
+                <Link href='/' className="btn btn-ghost normal-case text-xl">Games</Link>
+                <Link href='/about' className="btn btn-ghost normal-case text-xl">About</Link>
+                <Link href='/'className="btn btn-ghost normal-case text-xl">News</Link>
+                <Link href='/' className="btn btn-ghost normal-case text-xl">Community</Link>
+
+            </div>
+        </div>
+    </header>
+  );
+};
+
+export default Navbar;
