c321cc039722d8f31056a013e1481e5a2cdf16e7
Aaron Pierson <me@aaronpierson.me>
2023-11-15 22:37:40 -0600

    fixing code to read api markdown and images

--- app/news/[newsId]/page.tsx
+++ app/news/[newsId]/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from "next/link";
+import ReactMarkdown from 'react-markdown'
 
 interface Post {
     id: number;
@@ -8,6 +9,7 @@ interface Post {
     mainImage: string;
     date_created: string;
     date_updated: string;
+    test: string;
 }
 
 export default async function PostPage({ params }: { params: { newsId: string } }) {
@@ -15,15 +17,18 @@ export default async function PostPage({ params }: { params: { newsId: string } 
     const response = await fetch(`https://mana-news.astranexus.online/items/News/${params.newsId}`);
     const data = await response.json();
     const post: Post = data.data;  // Access the 'data' property of the response
+    const imgURL = "https://mana-news.astranexus.online/assets/"
     return (
         <div className="container m-10 mx-auto">
             <Link href="/news">
-                <p className="text-cyan-950 mt-5 hover:underline">Back to News</p>
+                <p className="text-cyan-500 mt-5 ml-5 hover:underline">Back to News</p>
             </Link>
-            <h1 className="text-3xl m-5 font-bold mb-8">{post.title}</h1>
             <p className="text-white m-5 text-sm mt-4">Posted on {new Date(post.date_created).toLocaleDateString("en-US")}</p>
-            <p className="text-white m-5">{post.content}</p>
-            {/* <Image src={post.mainImage} alt="News Image" width={400} height={400} /> */}
+            <h1 className="text-3xl m-5 font-bold mb-8">{post.title}</h1>
+            <Image className="m-auto" src={imgURL + post.mainImage} alt="News Image" width={400} height={400} />
+            <ReactMarkdown className="prose prose-sm sm:prose lg:prose-lg xl:prose-xl">
+                {post.content}
+            </ReactMarkdown>
         </div>
     );
 }
